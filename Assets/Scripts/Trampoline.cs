2e2e2352ffcc5153fb5034108da0c60be2c92ad2
Adafen <adafen@seznam.cz>
2026-04-21 14:59:52 +0200

    Všechny levely dokončeny

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -4,6 +4,12 @@ public class Trampoline : MonoBehaviour
 {
     [SerializeField] private float bounceForce = 10f;
 
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Check if the colliding object has a Rigidbody2D component
@@ -12,6 +18,13 @@ public class Trampoline : MonoBehaviour
         {
             // Apply an upward force to the Rigidbody2D to create a bouncing effect
             rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+
+            // Trigger the bounce animation
+            if (animator != null)
+            {
+                animator.SetTrigger("isBouncing");
+            }
+
         }
     }
 }
